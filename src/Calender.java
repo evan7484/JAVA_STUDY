b95a2cd8875ecf6ee25c30d46b52f13568ab03b1
junseo <evan7484@naver.com>
2022-07-30 17:05:43 +0900

    Update1_Using_Method

--- src/Calender.java
+++ src/Calender.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Calender {
+
+    private static final int[] Max_Days = {0, 31 ,28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+
+    public int GetMaxDays(int month){
+        return Max_Days[month];
+    }
     public static void main(String[] args) {
 
         System.out.println("일  월  화  수  목 금 토");
@@ -10,23 +16,14 @@ public class Calender {
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
 
-
         //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 
         System.out.println("달을 입력하세요: ");
+        Calender cal = new Calender();
         Scanner scanner = new Scanner(System.in);
         int Month = scanner.nextInt();
 
-        if ((Month == 1) | (Month == 3) | (Month == 5) | (Month == 7) | (Month == 8) | (Month == 10) | (Month == 12)) {
-            System.out.printf("%d월은 31일까지 있습니다.", Month);
-
-        } else if ((Month == 4) | (Month == 6) | (Month == 9) | (Month == 11)) {
-
-            System.out.printf("%d월은 30일까지 있습니다.", Month);
-        } else if ((Month == 2)) {
-
-            System.out.printf("%d월은 28일까지 있습니다.", Month);
-        }
+        System.out.printf("%d일은 %d일까지 있습니다.",Month, cal.GetMaxDays(Month));
         scanner.close();
     }
     }
